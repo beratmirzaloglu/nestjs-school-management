b23ab733e1f97a9479f73661b484d7b931127d5e
Berat Mirzaloğlu <berat.mirzaloglu@turkticaret.net>
2021-12-21 22:14:16 +0300

    typeorm db connection settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.DB_URL,
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     LessonModule,
     GraphQLModule.forRoot({
       autoSchemaFile: true,
